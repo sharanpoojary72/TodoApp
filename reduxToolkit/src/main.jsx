446a05725e5f458d43e62ab710b5caf04c52bcda
Sharan Poojary <sharanpoojary72@gmail.com>
2026-02-25 23:23:09 +0530

    cbr used

--- reduxToolkit/src/main.jsx
+++ reduxToolkit/src/main.jsx
@@ -1,11 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App'
 import { Provider } from 'react-redux'
-import {store} from './app/store'
+import { store } from './app/store'
+import Login from './components/Login'
+import Register from './components/Register'
+import ProtectedRoute from './components/ProtectedRoute'
+import Todos from './components/Todos'
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/todos",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/todos",
+        element: <Todos />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={route}></RouterProvider>
+    </Provider>
+  </StrictMode>,
 )
